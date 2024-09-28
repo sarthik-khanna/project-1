6893eb523279005e0a3ff979cc05e2a3e58cf84b
sarthik-khanna <164873613+sarthik-khanna@users.noreply.github.com>
2024-09-28 15:44:07 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Herosection from './component/hero';
+import Navigation from "./component/navigation";
+const App = () => {
+    return(
+         <div>
+<Navigation />
+<Herosection />
+    </div>
+    );
+};
+export default App;
